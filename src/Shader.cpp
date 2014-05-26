a0df72f1cf5f50f7f6742a9462a36900f545cade
ArthurSonzogni <Arthur.Sonzogni@ensimag.fr>
2014-05-26 14:06:51 +0200

    Shader : synchro avec Eduardo

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -7,7 +7,9 @@
 using namespace std;
 
 // collection des shader déja créés.
-map<string,Shader> shaderMap;
+map<string,Shader*> shaderMap;
+// collection des programmes déjà créés.
+map<string,ShaderProgram*> shaderProgramMap;
 
 // Lecture d'un fichier
 bool getFileContents(const char *filename, vector<char>& buffer)
@@ -38,7 +40,7 @@ Shader& Shader::loadFromFile(const char* filename, ShaderType::T type)
     // test si le shader est déja chargé en mémoire.
     auto it = shaderMap.find(string(filename));
     if (it!=shaderMap.end())
-        return it->second;
+        return *(it->second);
 
     // Sinon, on le charge
     Shader* s = new Shader();
@@ -48,7 +50,7 @@ Shader& Shader::loadFromFile(const char* filename, ShaderType::T type)
     if (not getFileContents(filename,fileContent))
     {
         cerr << "[Erreur] Fichier " << filename << " introuvable"<<endl;
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
     // creation
@@ -78,11 +80,11 @@ Shader& Shader::loadFromFile(const char* filename, ShaderType::T type)
         cerr<<log<<endl;
         cerr<<"============[Erreur]================="<<endl;
         
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
     // ajout du shader dans la map
-    //shaderMap[string(filename)]=s;
+    shaderMap[string(filename)]=s;
 
     return *s;
 }
@@ -92,8 +94,100 @@ Shader::Shader()
 
 }
 
+GLuint Shader::getHandle()
+{
+    return handle;
+}
+
 Shader::Shader(const Shader& shader)
 {
     handle = shader.handle;
-    uniformsMap = shader.uniformsMap;
+}
+
+
+ShaderProgram& ShaderProgram::loadFromFile(const char* vertexShader, const char* fragmentShader)
+{
+    return ShaderProgram::loadFromShader(
+                Shader::loadFromFile(vertexShader,ShaderType::Vertex),
+                Shader::loadFromFile(fragmentShader,ShaderType::Fragment)
+            );
+}
+
+ShaderProgram& ShaderProgram::loadFromShader(Shader& vertexShader, Shader& fragmentShader)
+{
+    // Si le program existe déjà on le renvoie
+    //auto it = shaderProgramMap.find
+
+    // programme creation
+    ShaderProgram* p = new ShaderProgram();
+	p->handle = glCreateProgram();
+    if (not p->handle)
+    {
+        cerr<<"[Erreur] Impossible de créer un ShaderProgramme vierge"<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+
+    // attachement.
+    glAttachShader(p->handle,vertexShader.getHandle());
+    glAttachShader(p->handle,fragmentShader.getHandle());
+
+    // linkage
+    glLinkProgram(p->handle);
+    GLint result;
+    glGetProgramiv(p->handle,GL_LINK_STATUS, &result);
+    if (result!=GL_TRUE)
+    {
+        cerr<<"[Erreur] Impossible de linker les shader";
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+GLuint ShaderProgram::uniform(const char* name)
+{
+
+}
+
+void ShaderProgram::setUniform(const char *name,float x,float y,float z)
+{
+
+}
+void ShaderProgram::setUniform(const char *name, const glm::vec3 & v)
+{
+
+}
+void ShaderProgram::setUniform(const char *name, const glm::vec4 & v)
+{
+
+}
+void ShaderProgram::setUniform(const char *name, const glm::mat4 & m)
+{
+
+}
+void ShaderProgram::setUniform(const char *name, const glm::mat3 & m)
+{
+
+}
+void ShaderProgram::setUniform(const char *name, float val )
+{
+
+}
+void ShaderProgram::setUniform(const char *name, int val )
+{
+
+}
+void ShaderProgram::setUniform(const char *name, bool val )
+{
+
+}
+
+ShaderProgram::ShaderProgram()
+{
+    
+}
+
+GLuint ShaderProgram::getHandle()
+{
+    return handle;
 }
--- src/Shader.h
+++ src/Shader.h
@@ -11,34 +11,32 @@
 #include <string>
 #include <map>
 
+class Shader;
+class ShaderProgram;
+
 namespace ShaderType
 {
     enum T {
-        VERTEX,
-        FRAGMENT,
-        GEOMETRY,
-        TESS_CONTROL,
-        TESS_EVALUATION
+        Vertex,
+        Fragment,
+        Geometry
     };
 };
 
-class Shader
+class ShaderProgram
 {
     public:
-        // Chargement du shader depuis un fichier
-        static Shader& loadFromFile(const char *filename,ShaderType::T type);
+        // constructeur
+        static ShaderProgram& loadFromFile(const char* vertexShader, const char* fragmentShader);
+        static ShaderProgram& loadFromShader(Shader& vertexShader, Shader& fragmentShader);
+
+        // Fournit l'identifiant du program
+        GLuint getHandle();
 
         // Fournit localisation d'un uniform.
         GLuint uniform(const char* name);
 
-        // Fournit l'identifiant du shader
-        GLuint getHandle();
-        
-
-        //void bindAttribLocation( GLuint location,const char * name);
-        //void bindFragDataLocation( GLuint location,const char * name );
-
-        // parametrer le shader
+        // parametrer le ShaderProgram
         void setUniform(const char *name,float x,float y,float z);
         void setUniform(const char *name, const glm::vec3 & v);
         void setUniform(const char *name, const glm::vec4 & v);
@@ -48,10 +46,37 @@ class Shader
         void setUniform(const char *name, int val );
         void setUniform(const char *name, bool val );
 
+        // parametrer les attributs
+        //void bindAttribLocation( GLuint location,const char * name);
+        //void bindFragDataLocation( GLuint location,const char * name );
     private:
-        Shader();   // constructeur privé, passer par Shader::fromFile(filename)
+        // constructeur (privé) . Utiliser loadFrom*
+        ShaderProgram();
+
+        // identifiant OpenGL
         GLuint handle;
+
+        // identifiants des attributs.
         std::map<std::string,GLuint> uniformsMap;
+};
+
+class Shader
+{
+    public:
+        // Chargement du shader depuis un fichier
+        static Shader& loadFromFile(const char *filename,ShaderType::T type);
+
+
+        // Fournit l'identifiant du shader
+        GLuint getHandle();
+        
+
+    private:
+        // constructeur privé, passer par Shader::fromFile(filename)
+        Shader();
+
+        // identifiant du shader OpenGL
+        GLuint handle;
 
         // constructeur par copie (private)
         Shader(const Shader& shader);
