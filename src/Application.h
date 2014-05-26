e28484f0cba053b86fce56b47966b36d0636d819
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-05-26 11:41:21 +0200

    Added Application class (singleton) + update cmakelist for twbar

---
+++ src/Application.h
@@ -0,0 +1,48 @@
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+/*******************************************************************************
+ *  Application                                                                *
+ *  Mon May 26 CEST 2014                                                       *
+ *  Copyright Eduardo San Martin Morote                                        *
+ *  eduardo.san-martin-morote@ensimag.fr                                       *
+ *  http://posva.net                                                           *
+ ******************************************************************************/
+
+#include "utils/Singleton.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#ifndef NTWBAR
+#include <AntTweakBar.h>
+#endif
+
+enum appState {
+    appReady,
+    appInLoop,
+    appPaused,
+    appExiting
+};
+
+class Application : public Singleton<Application> {
+    private:
+        Application();
+        appState state;
+        bool fullscreen, vsync, active;
+#ifndef NTWBAR
+        TwBar *bar;
+#endif
+        unsigned int width, height;
+        GLFWwindow* window;
+
+        friend Singleton<Application>;
+
+    public:
+        ~Application();
+        void run();
+        void loop();
+        //void switchFullscreen(bool fs);
+        //void resetVSync();
+        inline void exit() { state = appExiting; }
+
+};
+
+#endif
