90d40d92706f00838ff82310570cb23bf0ad5c2d
Jordan Rabet <jordan.rabet@ensimag.grenoble-inp.fr>
2014-05-27 10:46:27 +0200

    camera frustum

--- src/render/Camera.cpp
+++ src/render/Camera.cpp
@@ -1,6 +1,5 @@
 #include "render/Camera.h"
 #include "Application.h"
-#include "utils/glm.h"
 #include "utils/maths.h"
 
 Camera::Camera() :
@@ -12,6 +11,53 @@ Camera::Camera() :
 
 void Camera::updateCamera(ShaderProgram &prog)
 {
-    prog.setUniform("view", view);
-    prog.setUniform("proj", proj);
+	prog.setUniform("view", view);
+	prog.setUniform("proj", proj);
+}
+
+void Camera::updateFrustum(void)
+{
+	final=proj*view;
+
+	//near
+	frustumPlane[0]=
+		glm::vec4(final[2][0]+final[3][0],
+				final[2][1]+final[3][1],
+				final[2][2]+final[3][2],
+				final[2][3]+final[3][3]);
+
+	//far
+	frustumPlane[1]=
+		glm::vec4(-final[2][0]+final[3][0],
+				-final[2][1]+final[3][1],
+				-final[2][2]+final[3][2],
+				final[2][3]+final[3][3]);
+
+	//left
+	frustumPlane[2]=
+		glm::vec4(final[0][0]+final[3][0],
+				final[0][1]+final[3][1],
+				final[0][2]+final[3][2],
+				final[0][3]+final[3][3]);
+
+	//right
+	frustumPlane[3]=
+		glm::vec4(-final[0][0]+final[3][0],
+				-final[0][1]+final[3][1],
+				-final[0][2]+final[3][2],
+				final[0][3]+final[3][3]);
+
+	//bottom
+	frustumPlane[4]=
+		glm::vec4(final[1][0]+final[3][0],
+				final[1][1]+final[3][1],
+				final[1][2]+final[3][2],
+				final[1][3]+final[3][3]);
+
+	//top
+	frustumPlane[5]=
+		glm::vec4(-final[1][0]+final[3][0],
+				-final[1][1]+final[3][1],
+				-final[1][2]+final[3][2],
+				final[1][3]+final[3][3]);	
 }
--- src/render/Camera.h
+++ src/render/Camera.h
@@ -1,17 +1,19 @@
 #ifndef __CAMERA_H__
 #define __CAMERA_H__
 
-#define GLM_FORCE_RADIANS
-#include <glm/mat4x4.hpp>
+#include "utils/glm.h"
 #include "render/Shader.h"
 
 class Camera {
-    public:
-        glm::mat4 view, proj;
+	public:
+		glm::mat4 view, proj, final;
 
-        Camera();
-        // the shader needs to have 2 uniforms: view and proj
-        void updateCamera(ShaderProgram &prog);
+		Camera();
+		// the shader needs to have 2 uniforms: view and proj
+		void updateCamera(ShaderProgram &prog);
+		void updateFrustum(void);
+	private:
+		glm::vec4 frustumPlane[6]; 
 };
 
 #endif
