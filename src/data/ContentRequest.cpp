ad15a3cc118113d1f9cd01e56bca9725e10c5666
smea <smealum@gmail.com>
2014-05-31 17:59:53 +0200

    WIP geoCHUNK

--- src/data/ContentRequest.cpp
+++ src/data/ContentRequest.cpp
@@ -68,38 +68,16 @@ void WorldChunkRequest::computeChunk(void)
 			{
 				GL_Vertex v;
 				v.facedir=2;
-				v.position=vec3(px+x,py+y,pz+z);     vArray.push_back(v);
-				// v.color=vec4(
-				// 	double(x)/double(CHUNK_N),
-				// 	double(y)/double(CHUNK_N),
-				// 	double(z)/double(CHUNK_N),
-				// 	1.0);
-				// v.normal=vec3(-1,0,0);
-				// v.position=vec3(x,y,z)*BLOCK_SIZE;     vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z+1)*BLOCK_SIZE; vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
+				v.position=vec3(px+x,py+y,pz+z);
+				vArray.push_back(v);
 			}
 		}else{
 			if (data[x-1][y][z])
 			{
 				GL_Vertex v;
 				v.facedir=3;
-				v.position=vec3(px+x-1,py+y,pz+z);     vArray.push_back(v);
-				// v.color=vec4(
-				// 	double(x-1)/double(CHUNK_N),
-				// 	double(y)/double(CHUNK_N),
-				// 	double(z)/double(CHUNK_N),
-				// 	1.0);
-				// v.normal=vec3(1,0,0);
-				// v.position=vec3(x,y,z)*BLOCK_SIZE;     vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z+1)*BLOCK_SIZE; vArray.push_back(v);
+				v.position=vec3(px+x-1,py+y,pz+z);
+				vArray.push_back(v);
 			}
 		}
 	}
@@ -115,35 +93,16 @@ void WorldChunkRequest::computeChunk(void)
 			{
 				GL_Vertex v;
 				v.facedir=0;
-				v.position=vec3(px+x,py+y,pz+z);     vArray.push_back(v);
-				// v.color=vec4(
-				// double(x)/double(CHUNK_N),
-				// double(y)/double(CHUNK_N),
-				// double(z)/double(CHUNK_N),
-				// 1.0
-				// );
-				// v.normal=vec3(0,-1,0);
-				// v.position=vec3(x,y,z)*BLOCK_SIZE;     vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z+1)*BLOCK_SIZE; vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
+				v.position=vec3(px+x,py+y,pz+z);
+				vArray.push_back(v);
 			}
 		}else{
 			if (data[x][y-1][z])
 			{
 				GL_Vertex v;
 				v.facedir=1;
-				v.position=vec3(px+x,py+y-1,pz+z);     vArray.push_back(v);
-				// v.position=vec3(px+x,0,pz+z);     vArray.push_back(v);
-				// v.position=vec3(PLANETFACE_BLOCKS/2,0,PLANETFACE_BLOCKS/2);     vArray.push_back(v);
-				// printf("x%d y%d\n",px+x,PLANETFACE_BLOCKS/2);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y,z+1)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z+1)*BLOCK_SIZE; vArray.push_back(v);
+				v.position=vec3(px+x,py+y-1,pz+z);
+				vArray.push_back(v);
 			}
 		}
 	}
@@ -159,38 +118,16 @@ void WorldChunkRequest::computeChunk(void)
 			{
 				GL_Vertex v;
 				v.facedir=4;
-				v.position=vec3(px+x,py+y,pz+z);     vArray.push_back(v);
-				// v.color=vec4(
-				// 	double(x)/double(CHUNK_N),
-				// 	double(y)/double(CHUNK_N),
-				// 	double(z)/double(CHUNK_N),
-				// 	1.0);
-				// v.normal=vec3(0,0,-1);
-				// v.position=vec3(x,y,z)*BLOCK_SIZE;     vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y+1,z)*BLOCK_SIZE; vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
+				v.position=vec3(px+x,py+y,pz+z);
+				vArray.push_back(v);
 			}
 		}else{
 			if (data[x][y][z-1])
 			{
 				GL_Vertex v;
 				v.facedir=5;
-				v.position=vec3(px+x,py+y,pz-1+z);     vArray.push_back(v);
-				// v.color=vec4(
-				// 	double(x)/double(CHUNK_N),
-				// 	double(y)/double(CHUNK_N),
-				// 	double(z-1)/double(CHUNK_N),
-				// 	1.0);
-				// v.normal=vec3(0,0,1);
-				// v.position=vec3(x,y,z)*BLOCK_SIZE;     vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x,y+1,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y,z)*BLOCK_SIZE;   vArray.push_back(v);
-				// v.position=vec3(x+1,y+1,z)*BLOCK_SIZE; vArray.push_back(v);
+				v.position=vec3(px+x,py+y,pz-1+z);
+				vArray.push_back(v);
 			}
 		}
 	}
@@ -205,13 +142,13 @@ void WorldChunkRequest::process(void)
 	{
 		for(int k=0;k<CHUNK_N;k++)
 		{
-			// const int h=(int)((glm::simplex(glm::vec2(px*CHUNK_N+i,pz*CHUNK_N+k)*0.01f)+1.0f)*32);
-			const glm::vec3 v=(origin+(float(px*CHUNK_N+i)/MINIWORLD_N)*(v1)+(float(pz*CHUNK_N+k)/MINIWORLD_N)*(v2))*1000.0f;
-			const int h=(int)((glm::simplex(v)+1.0f)*32);
-			// const int h=(int)((getElevation(v)-elevation)*320);
-			// printf("h %f\n",f);
-			// const int h=CHUNK_N/2;
-			// const int h=2;
+			// // const int h=(int)((glm::simplex(glm::vec2(px*CHUNK_N+i,pz*CHUNK_N+k)*0.01f)+1.0f)*32);
+			// const glm::vec3 v=(origin+(float(px*CHUNK_N+i)/MINIWORLD_N)*(v1)+(float(pz*CHUNK_N+k)/MINIWORLD_N)*(v2))*1000.0f;
+			// const int h=(int)((glm::simplex(v)+1.0f)*32);
+			// // const int h=(int)((getElevation(v)-elevation)*320);
+			// // printf("h %f\n",f);
+			// // const int h=CHUNK_N/2;
+			const int h=2;
 			for(int j=0;j<CHUNK_N;j++)
 			{
 				if(py*CHUNK_N+j<=h)data[i][j][k]=1;
