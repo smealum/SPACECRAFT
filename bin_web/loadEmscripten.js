8cd6ab8909c20abcb8237cf727d1c7d178223aff
ArthurSonzogni <Arthur.Sonzogni@grenoble-inp.org>
2017-12-13 01:48:01 +0100

    Fix errors in loadEmpscripten.js

--- bin_web/loadEmscripten.js
+++ bin_web/loadEmscripten.js
@@ -1,39 +1,28 @@
-var Module = {
-  preRun: [],
-  postRun: [],
+var Module =
+{
+    preRun: [],
+    postRun: [],
+    print: (function() {
+        var element = document.getElementById('output');
+        if (element) element.value = ''; // clear browser cache
+        return function(text) {
+            if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+            console.log(text);
+            if (element) {
+                element.value += text + "\n";
+                element.scrollTop = element.scrollHeight; // focus on bottom
+            }
+        };
+    })(),
+    printErr: function(text)
+    {
+        if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+            console.error(text);
+    },
+    canvas: (function() {
+      var canvas = document.getElementById('canvas');
+      canvas.addEventListener("webglcontextlost", function(e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
 
-  print: (function() {
-    var output = document.getElementById('output');
-    if (output)
-      output.value = ''; // clear browser cache
-
-    return function(text) {
-      if (arguments.length > 1)
-        text = Array.prototype.slice.call(arguments).join(' ');
-
-      // Log output in the console and in the <textarea id="output"> element.
-      console.log(text);
-      if (output) {
-        output.value += text + "\n";
-        output.scrollTop = output.scrollHeight; // focus on bottom
-      }
-    };
-  })(),
-
-  printErr: function(text) {
-    if (arguments.length > 1)
-      text = Array.prototype.slice.call(arguments).join(' ');
-    console.error(text);
-  },
-
-  canvas: (function() {
-    var canvas = document.getElementById('canvas');
-    var context_lost_handler = function(e) {
-      alert('WebGL context lost. You will need to reload the page.');
-      e.preventDefault();
-    }
-    canvas.addEventListener("webglcontextlost", e, false);
-
-    return canvas;
-  })(),
+      return canvas;
+    })(),
 };
